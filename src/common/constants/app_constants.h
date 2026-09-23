813bb305f329c11975b785ea9ef5603f88487e41
David Taylor <davidtaylor613@gmail.com>
2026-09-23 23:50:25 +0100

    Prepare beta-16 desktop and remote helper build

--- src/common/constants/app_constants.h
+++ src/common/constants/app_constants.h
@@ -21,7 +21,7 @@ namespace uam::constants
 	/// <summary>
 	/// User-facing application version string.
 	/// </summary>
-	inline constexpr const char* kAppVersion = "V4.9.0-beta-15";
+	inline constexpr const char* kAppVersion = "V4.9.0-beta-16";
 
 	/// <summary>
 	/// Copyright footer displayed in About dialogs.
