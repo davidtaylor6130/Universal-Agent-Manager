cbb4fb7125b37ab45fb7a9c69077de74dac60cbd
David Taylor <davidtaylor613@gmail.com>
2026-09-23 22:47:15 +0100

    Prepare beta-14 remote helper fix

--- src/common/constants/app_constants.h
+++ src/common/constants/app_constants.h
@@ -21,7 +21,7 @@ namespace uam::constants
 	/// <summary>
 	/// User-facing application version string.
 	/// </summary>
-	inline constexpr const char* kAppVersion = "V4.9.0-beta-13";
+	inline constexpr const char* kAppVersion = "V4.9.0-beta-14";
 
 	/// <summary>
 	/// Copyright footer displayed in About dialogs.
