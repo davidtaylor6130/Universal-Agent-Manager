650b94cfacb51ab347b75d4ad8fd722d678868e7
David Taylor <davidtaylor613@gmail.com>
2026-09-23 22:28:19 +0100

    Prepare beta-13 desktop and helper build

--- src/common/constants/app_constants.h
+++ src/common/constants/app_constants.h
@@ -21,7 +21,7 @@ namespace uam::constants
 	/// <summary>
 	/// User-facing application version string.
 	/// </summary>
-	inline constexpr const char* kAppVersion = "V4.9.0-beta-12";
+	inline constexpr const char* kAppVersion = "V4.9.0-beta-13";
 
 	/// <summary>
 	/// Copyright footer displayed in About dialogs.
