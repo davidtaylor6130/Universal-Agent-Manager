84ebb3bdda29b488404d56c271b8fc19288a001d
David Taylor <davidtaylor613@gmail.com>
2026-09-23 20:27:33 +0100

    Identify validated build as beta 11

--- src/common/constants/app_constants.h
+++ src/common/constants/app_constants.h
@@ -21,7 +21,7 @@ namespace uam::constants
 	/// <summary>
 	/// User-facing application version string.
 	/// </summary>
-	inline constexpr const char* kAppVersion = "V4.9.0-beta-10";
+	inline constexpr const char* kAppVersion = "V4.9.0-beta-11";
 
 	/// <summary>
 	/// Copyright footer displayed in About dialogs.
