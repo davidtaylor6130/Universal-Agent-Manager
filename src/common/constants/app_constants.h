352fff327436b6544d125ab4fa0974a4b99891a0
David Taylor <davidtaylor613@gmail.com>
2026-09-24 17:06:52 +0100

    Package UAM beta29 for desktop and SSH helpers

--- src/common/constants/app_constants.h
+++ src/common/constants/app_constants.h
@@ -21,7 +21,7 @@ namespace uam::constants
 	/// <summary>
 	/// User-facing application version string.
 	/// </summary>
-	inline constexpr const char* kAppVersion = "V4.9.0-beta-28";
+	inline constexpr const char* kAppVersion = "V4.9.0-beta-29";
 
 	/// <summary>
 	/// Copyright footer displayed in About dialogs.
