a14945dcd72ec599a2cca6178bb8fa6c39dfb765
David Taylor <davidtaylor613@gmail.com>
2026-09-24 14:15:17 +0100

    Package beta25 with remote prompt recovery

--- src/common/constants/app_constants.h
+++ src/common/constants/app_constants.h
@@ -21,7 +21,7 @@ namespace uam::constants
 	/// <summary>
 	/// User-facing application version string.
 	/// </summary>
-	inline constexpr const char* kAppVersion = "V4.9.0-beta-24";
+	inline constexpr const char* kAppVersion = "V4.9.0-beta-25";
 
 	/// <summary>
 	/// Copyright footer displayed in About dialogs.
