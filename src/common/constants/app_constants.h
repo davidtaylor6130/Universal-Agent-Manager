70d3c53f9ca27adbe79e3cf1a729371df11ce18e
David Taylor <davidtaylor613@gmail.com>
2026-09-24 13:40:26 +0100

    Package beta24 reliability fixes

--- src/common/constants/app_constants.h
+++ src/common/constants/app_constants.h
@@ -21,7 +21,7 @@ namespace uam::constants
 	/// <summary>
 	/// User-facing application version string.
 	/// </summary>
-	inline constexpr const char* kAppVersion = "V4.9.0-beta-23";
+	inline constexpr const char* kAppVersion = "V4.9.0-beta-24";
 
 	/// <summary>
 	/// Copyright footer displayed in About dialogs.
